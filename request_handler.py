374a56f26fdda147883ed50b7607249867bb8c55
Emily Drew L <DontDenyHighFives@gmail.com>
2024-04-18 16:45:13 -0600

    chapter 5 and 6 finished

--- request_handler.py
+++ request_handler.py
@@ -4,15 +4,23 @@ from views import (
   get_all_animals,
   get_single_animal,
   create_animal,
+  delete_animal,
+  update_animal,
   get_all_locations,
   get_single_location,
   create_location,
+  delete_location,
+  update_location,
   get_all_employees,
   get_single_employee,
   create_employee,
+  delete_employee,
+  update_employee,
   get_all_customers,
   get_single_customer,
-  create_customer
+  create_customer,
+  delete_customer,
+  update_customer
 )
 
 # Here's a class. It inherits from another class.
@@ -152,7 +160,54 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_PUT(self):
         """Handles PUT requests to the server
         """
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        if resource == "locations":
+            update_location(id, post_body)
+
+        if resource == "employees":
+            update_employee(id, post_body)
+
+        if resource == "customers":
+            update_customer(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
         self.do_POST()
+
+
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        if resource == "locations":
+            delete_location(id)
+
+        if resource == "employees":
+            delete_employee(id)
+
+        if resource == "customers":
+            delete_customer(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
 
 # This function is not inside the class. It is the starting
--- views/location_requests.py
+++ views/location_requests.py
@@ -39,3 +39,25 @@ def create_location(location):
     LOCATIONS.append(location)
 
     return location
+
+def update_location(id, new_location):
+
+    for index, location in enumerate(LOCATIONS):
+        if location["id"] == id:
+
+            LOCATIONS[index] = new_location
+            break
+
+def delete_location(id):
+
+    location_index = -1
+
+
+    for index, location in enumerate(LOCATIONS):
+        if location["id"] == id:
+
+            location_index = index
+
+
+    if location_index >= 0:
+        LOCATIONS.pop(location_index)
